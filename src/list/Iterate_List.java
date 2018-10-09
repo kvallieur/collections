94ccdb770440b407909373de1642d2093bfbf35e
Karthik Musunur <musunur.karthik@gmail.com>
2018-10-09 10:13:32 -0500

    Update and rename SortListJava8.java to Iterate_List.java

--- src/list/Iterate_List.java
+++ src/list/Iterate_List.java
@@ -8,13 +8,22 @@ public class SortListJava8 {
 	public static void main(String[] args) {
 		List<Integer> numbers = new LinkedList<>();
 
-		// Adding
+		// Adding elements to List
 		numbers.add(10);
 		numbers.add(500);
-		numbers.add(87);
+		numbers.add(14);
+		numbers.add(31);
+		numbers.add(17);
+		numbers.add(23);
+		numbers.add(41);
+		numbers.add(114);
+		numbers.add(211);
+		numbers.add(30);
+		numbers.add(2);
+		numbers.add(4);
 
-		// Retrieving
-		System.out.println(numbers.get(2));
+		// Retrieving - get the nth element in List
+		System.out.println(numbers.get(2) + " 2nd element is");
 
 		// Iterating in java 8 with lambda
 		numbers.forEach(number -> System.out.println(number));
